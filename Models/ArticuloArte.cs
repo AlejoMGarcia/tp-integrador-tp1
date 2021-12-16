573b25cf29a0c190c8da79da612fe2d346b0116e
Alejo <ale.garciaa210@gmail.com>
2021-12-16 18:35:01 -0300

    Refactor en models y vistas

--- Models/ArticuloArte.cs
+++ Models/ArticuloArte.cs
@@ -8,10 +8,14 @@ namespace MVCBasico.Models
 {
     public class ArticuloArte : Articulo
     {
+        [Display(Name = "Artista"),
+        Required(ErrorMessage = "Por favor ingrese un artista.")]
         public string Artista { get; set; }
-        [Display(Name = "Período")]
+        [Display(Name = "Período"),
+        Required(ErrorMessage = "Por favor ingrese un período.")]
         public string Periodo { get; set; }
-        [Display(Name = "Tipo de Arte")]
+        [Display(Name = "Tipo de Arte"),
+        Required(ErrorMessage = "Por favor ingrese un tipo de arte.")]
         public TipoArte TipoArte { get; set; }
     }
 }
--- Models/ArticuloMueble.cs
+++ Models/ArticuloMueble.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,23 @@ namespace MVCBasico.Models
 {
     public class ArticuloMueble : Articulo
     {
+        [Display(Name = "Alto"),
+        Required(ErrorMessage = "Por favor ingrese un alto.")]
         public double Alto { get; set; }
+        [Display(Name = "Ancho"),
+        Required(ErrorMessage = "Por favor ingrese un ancho.")]
         public double Ancho { get; set; }
+        [Display(Name = "Materia"),
+        Required(ErrorMessage = "Por favor ingrese un material.")]
         public string Material { get; set; }
+        [Display(Name = "Peso"),
+        Required(ErrorMessage = "Por favor ingrese un peso.")]
         public double Peso { get; set; }
+        [Display(Name = "Tipo"),
+        Required(ErrorMessage = "Por favor ingrese un tipo.")]
         public string Tipo { get; set; }
+        [Display(Name = "Fabricante"),
+        Required(ErrorMessage = "Por favor ingrese un fabricante.")]
         public string Fabricante { get; set; }
     }
 }
